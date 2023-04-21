a9f0ddea1757d2cde6f5b503b166d848267be224
Zudah228 <rezel95.hin@gmail.com>
2023-04-21 18:10:56 +0900

    Amplify DataStore の追加

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "flutterawsworkspace": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_aws_workspace/amplifyconfiguration.dart';
+import 'package:flutter_aws_workspace/models/ModelProvider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Amplify
+  try {
+    final amplifyAPI = AmplifyAPI(modelProvider: ModelProvider.instance);
+    final amplifyDataStore =
+        AmplifyDataStore(modelProvider: ModelProvider.instance);
+    await Amplify.addPlugins([amplifyAPI, amplifyDataStore]);
+    await Amplify.configure(amplifyconfig);
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+
   runApp(const MyApp());
 }
 
@@ -29,16 +49,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final GlobalKey<FormFieldState> _formFieldKey = GlobalKey<FormFieldState>();
+  List<Todo> _todos = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<void> _fetch() async {
+    final items = await Amplify.DataStore.query(Todo.classType);
+    if (items.any((e) => e.createdAt == null)) {
+      _fetch();
+    } else {
+      setState(() {
+        _todos = [...items];
+      });
+    }
+  }
+
+  Future<void> _save() async {
+    try {
+      if (!_formFieldKey.currentState!.validate()) return;
+      final name = _formFieldKey.currentState!.value;
+
+      await Amplify.DataStore.save<Todo>(Todo(name: name, isComplete: false));
+      _formFieldKey.currentState!.reset();
+      _fetch();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   @override
   void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _fetch();
+    });
     super.initState();
   }
 
@@ -48,24 +90,39 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextFormField(
+                    key: _formFieldKey,
+                    validator: (value) => value!.isEmpty ? 'required' : null,
+                  )),
+                  const SizedBox(width: 8),
+                  ElevatedButton(onPressed: _save, child: const Icon(Icons.add))
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              final todo = _todos[index];
+              return ListTile(
+                title: Text(todo.name ?? ''),
+                subtitle: Text(todo.createdAt?.format() ?? ''),
+              );
+            }, childCount: _todos.length),
+          )
+        ]
+            .map((e) => SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 12), sliver: e))
+            .toList(),
       ),
     );
   }
